acd9ba91577769d4537b09518c9e0afbfed2d989
PritomKumar <bsse0919@iit.du.ac.bd>
2020-09-21 00:43:16 +0600

    Finish codeforces 465B

--- Inbox (100500).cpp
+++ Inbox (100500).cpp
@@ -2,16 +2,60 @@
 
 using namespace std;
 
-int main() {
+int findFirstOne(int letters[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        if (letters[i] == 1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int findLastOne(int letters[], int n)
+{
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (letters[i] == 1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int letters[n];
     for (int i = 0; i < n; i++)
     {
         cin >> letters[i];
     }
 
-    
-    
+    int first = findFirstOne(letters, n);
+    int last = findLastOne(letters, n);
+    int counter = 0;
+    if (first != -1)
+    {
+        for (int i = first; i <= last; i++)
+        {
+            counter++;
+            if (letters[i] == 0)
+            {
+
+                counter++;
+                while (letters[i] == 0)
+                {
+                    i++;
+                }
+            }
+        }
+    }
+
+    cout << counter << endl;
     return 0;
 }
