d6caf1e500c3f0891d220827ff436fdf682c56ed
PritomKumar <bsse0919@iit.du.ac.bd>
2020-10-18 19:24:41 +0600

    Solved in one go

--- Sequential Nim.cpp
+++ Sequential Nim.cpp
@@ -13,6 +13,23 @@ int main() {
         {
             cin>>arr[i];
         }
+
+        bool first = true;
+        bool unBreak = false;
+        for (int i = 0; i < n; i++)
+        {
+            if(arr[i]>1){
+                first ? cout << "First" <<endl: cout << "Second" <<endl;
+                unBreak = true;
+                break;
+            }
+            else{
+                first = !first;
+            }
+        }
+        if(!unBreak){
+            !first ? cout << "First" <<endl: cout << "Second" <<endl;
+        }
         
     }
     return 0;
