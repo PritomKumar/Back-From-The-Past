40a760966760b3bdc8137032c094b3562d5c0751
PritomKumar <bsse0919@iit.du.ac.bd>
2020-12-22 09:15:34 +0600

    1200 level problem start

--- Maximum Product.cpp
+++ Maximum Product.cpp
@@ -2,7 +2,73 @@
 
 using namespace std;
 
-int main() {
-    
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int numbers[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &numbers[i]);
+        }
+
+        int p = 0;
+        int maximumNumbers[5];
+        while (p < 5)
+        {
+
+            int maxi = 0;
+            int maxiIndex = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (maxi <= abs(numbers[i]))
+                {
+                    maxi = numbers[i];
+                    maxiIndex = i;
+                }
+            }
+            maximumNumbers[p] = maxi;
+            numbers[maxiIndex] = 0;
+            p++;
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            cout <<  maximumNumbers[i]<< " ";
+        }
+
+        long long int answer = 1;
+        for (int i = 0; i < n; i++)
+        {
+            answer*= maximumNumbers[i];
+        }
+        
+        cout << answer <<endl;
+
+
+        //sort(numbers, numbers + n);
+        // if (n > 10)
+        // {
+        //     int maximumArray[10];
+        //     for (int i = 0, j = n - 1, k=9; i < 5; i++, j--,k--)
+        //     {
+        //         maximumArray[i] = numbers[i];
+        //         maximumArray[k] = numbers[j];
+        //     }
+        // }
+        // else{
+        //     int newArray[n];
+        //     for (int i = 0; i < n; i++)
+        //     {
+
+        //     }
+
+        // }
+    }
     return 0;
 }
--- aaaaaaamain copy 3.cpp
+++ aaaaaaamain copy 3.cpp
@@ -2,7 +2,101 @@
 
 using namespace std;
 
-int main() {
-    
+struct Point
+{
+    int x;
+    int y;
+    static bool comparex(const Point a, const Point b)
+    {
+        return a.x < b.x;
+    }
+    static bool comparey(const Point a, const Point b)
+    {
+        return a.y < b.y;
+    }
+};
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        struct Point grid[n];
+        struct Point grid2[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> grid[i].x >> grid[i].y;
+            grid2[i].x = grid[i].x;
+            grid2[i].y = grid[i].y;
+        }
+
+        sort(grid, grid + n, Point::comparex);
+        sort(grid, grid + n, Point::comparey);
+
+        sort(grid2, grid2 + n, Point::comparey);
+        sort(grid2, grid2 + n, Point::comparex);
+
+        string answer = "";
+        bool check = true;
+        Point initialPoint;
+        initialPoint.x = 0;
+        initialPoint.y = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (grid[i].x >= initialPoint.x && grid[i].y >= initialPoint.y)
+            {
+                for (int i = 0; i < grid[i].x - initialPoint.x; i++)
+                {
+                    answer += "R";
+                }
+
+                for (int i = 0; i < grid[i].y - initialPoint.y; i++)
+                {
+                    answer += "U";
+                }
+            }
+            else
+            {
+                check = false;
+                break;
+            }
+        }
+
+        if (!check)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (grid[i].x >= initialPoint.x && grid[i].y >= initialPoint.y)
+                {
+                    for (int i = 0; i < grid[i].x - initialPoint.x; i++)
+                    {
+                        answer += "R";
+                    }
+
+                    for (int i = 0; i < grid[i].y - initialPoint.y; i++)
+                    {
+                        answer += "U";
+                    }
+                }
+                else
+                {
+                    check = false;
+                    break;
+                }
+            }
+        }
+        if(check){
+            cout << "YES"<<endl;
+            cout << answer<<endl;
+        }
+        else{
+             cout << "NO"<<endl;
+        }
+    }
     return 0;
 }
