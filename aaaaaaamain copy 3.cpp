d19bbb1f03d5e4c2aea5ce87bed98863381a23c6
PritomKumar <bsse0919@iit.du.ac.bd>
2020-12-22 09:40:00 +0600

    new approach

--- aaaaaaamain copy 3.cpp
+++ aaaaaaamain copy 3.cpp
@@ -25,78 +25,146 @@ int main()
     {
         int n;
         cin >> n;
-        struct Point grid[n];
-        struct Point grid2[n];
 
+        bool grid[1010][1010];
+        for (int i = 0; i < 1010; i++)
+        {
+            for (int j = 0; j < 1010; j++)
+            {
+                grid[i][j] = false;
+            }
+        }
         for (int i = 0; i < n; i++)
         {
-            cin >> grid[i].x >> grid[i].y;
-            grid2[i].x = grid[i].x;
-            grid2[i].y = grid[i].y;
+            int x, y;
+            cin >> x >> y;
+            grid[x][y] = true;
         }
 
-        sort(grid, grid + n, Point::comparex);
-        sort(grid, grid + n, Point::comparey);
-
-        sort(grid2, grid2 + n, Point::comparey);
-        sort(grid2, grid2 + n, Point::comparex);
-
-        string answer = "";
+        int prex = 0, prey = 0;
         bool check = true;
-        Point initialPoint;
-        initialPoint.x = 0;
-        initialPoint.y = 0;
-        for (int i = 0; i < n; i++)
+        string answer = "";
+        for (int i = 0; i < 1010; i++)
         {
-            if (grid[i].x >= initialPoint.x && grid[i].y >= initialPoint.y)
+            for (int j = 0; j < i; j++)
             {
-                for (int i = 0; i < grid[i].x - initialPoint.x; i++)
+                if (grid[i][j])
                 {
-                    answer += "R";
-                }
-
-                for (int i = 0; i < grid[i].y - initialPoint.y; i++)
-                {
-                    answer += "U";
-                }
-            }
-            else
-            {
-                check = false;
-                break;
-            }
-        }
-
-        if (!check)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (grid[i].x >= initialPoint.x && grid[i].y >= initialPoint.y)
-                {
-                    for (int i = 0; i < grid[i].x - initialPoint.x; i++)
+                    if (prex < i || prey < j)
                     {
-                        answer += "R";
+                        check = false;
+                        break;
                     }
-
-                    for (int i = 0; i < grid[i].y - initialPoint.y; i++)
+                    else
                     {
-                        answer += "U";
+                        for (int k = 0; k < i - prex; k++)
+                        {
+                            answer += "R";
+                        }
+
+                        for (int k = 0; k < j - prey; k++)
+                        {
+                            answer += "U";
+                        }
+                        prex = i;
+                        prey = j;
+                        check = true;
                     }
                 }
-                else
-                {
-                    check = false;
-                    break;
-                }
             }
         }
-        if(check){
-            cout << "YES"<<endl;
-            cout << answer<<endl;
+
+        if (check)
+        {
+            cout << "YES" << endl;
+            cout << answer << endl;
         }
-        else{
-             cout << "NO"<<endl;
+        else
+        {
+            cout << "NO" << endl;
         }
+        // struct Point grid[n];
+        // struct Point grid2[n];
+
+        // for (int i = 0; i < n; i++)
+        // {
+        //     cin >> grid[i].x >> grid[i].y;
+        //     grid2[i].x = grid[i].x;
+        //     grid2[i].y = grid[i].y;
+        // }
+
+        // sort(grid, grid + n, Point::comparex);
+        // sort(grid, grid + n, Point::comparey);
+
+        // sort(grid2, grid2 + n, Point::comparey);
+        // sort(grid2, grid2 + n, Point::comparex);
+
+        // string answer = "";
+        // bool check = true;
+        // Point initialPoint;
+        // initialPoint.x = 0;
+        // initialPoint.y = 0;
+        // for (int i = 0; i < n; i++)
+        // {
+        //     if (grid[i].x >= initialPoint.x && grid[i].y >= initialPoint.y)
+        //     {
+        //         for (int k = 0; j < grid[i].x - initialPoint.x; j++)
+        //         {
+        //             answer += "R";
+        //         }
+
+        //         for (int j = 0; j < grid[i].y - initialPoint.y; j++)
+        //         {
+        //             answer += "U";
+        //         }
+        //         initialPoint.x = grid[i].x;
+        //         initialPoint.y = grid[i].y;
+        //         check = true;
+        //     }
+        //     else
+        //     {
+        //         check = false;
+        //         break;
+        //     }
+        // }
+
+        // if (!check)
+        // {
+        //     answer = "";
+        //     for (int i = 0; i < n; i++)
+        //     {
+        //         if (grid2[i].x >= initialPoint.x && grid2[i].y >= initialPoint.y)
+        //         {
+        //             for (int j = 0; j < grid2[i].x - initialPoint.x; j++)
+        //             {
+        //                 answer += "R";
+        //             }
+
+        //             for (int j = 0; j < grid2[i].y - initialPoint.y; j++)
+        //             {
+        //                 answer += "U";
+        //             }
+        //             initialPoint.x = grid2[i].x;
+        //             initialPoint.y = grid2[i].y;
+        //             check = true;
+
+        //         }
+        //         else
+        //         {
+        //             check = false;
+        //             break;
+        //         }
+        //     }
+        // }
+        // if (check)
+        // {
+        //     cout << "YES" << endl;
+        //     cout << answer << endl;
+        // }
+        // else
+        // {
+        //     cout << "NO" << endl;
+        // }
     }
     return 0;
 }
